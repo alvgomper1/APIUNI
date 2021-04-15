460616ccc5e5e31755e1cbed91c03ef9c4215181
alvaro <alvgomper1@gmail.com>
2021-04-15 11:54:37 +0200

    Primer commit de proyecto base

---
+++ apiuni/src/main/java/com/apiuni/apiuni/ApiuniApplication.java
@@ -0,0 +1,13 @@
+package com.apiuni.apiuni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiuniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiuniApplication.class, args);
+	}
+
+}
---
+++ apiuni/src/test/java/com/apiuni/apiuni/ApiuniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiuni.apiuni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiuniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
